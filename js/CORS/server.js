f4f06b2c3b2b0945875cfb2d19e9cdd8b55b4ef6
“fanhuanxi” <fanhuanxi@meituan.com>
2021-06-24 20:22:21 +0800

    CORS

--- js/CORS/server.js
+++ js/CORS/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3005
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1')
