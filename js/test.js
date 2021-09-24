80596138b77e8a47ebb9e4ce31c9b88245f66fc2
“fanhuanxi” <fanhuanxi@meituan.com>
2021-09-24 14:49:41 +0800

    9.24

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
-let str = 'hello worlld!'
-console.log(str.indexOf('ll')) // 返回第一个匹配字符的位置，-1表示无匹配
-// 返回布尔值，第二个参数表示开始搜索的位置
-console.log(str.includes('ll', 1))
-console.log(str.startsWith('hell'))
-// 返回布尔值，第二个参数表示在前n个字符中查找
-console.log(str.endsWith('world', 7))
+var a = {n:1}; 
+var b = a; 
+a.x = a = {n:2} 
+console.log(a.x); 
+console.log(b.x); 
+console.log(a); 
+console.log(b);
