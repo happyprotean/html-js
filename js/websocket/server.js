f05afc8b95c352ae30fbc8baba87bfbe7212cfe3
“fanhuanxi” <fanhuanxi@meituan.com>
2021-11-01 14:58:06 +0800

    11.1

--- js/websocket/server.js
+++ js/websocket/server.js
@@ -2,9 +2,23 @@ import express from 'express'
 import WebSocket from 'ws'
 let app = express();
 let wss = new WebSocket.Server({port:3000});
+
 wss.on('connection',function(ws) {
+  ws.send('Hi')
+  console.log('我：Hi')
   ws.on('message', function (data) {
-    console.log(data);
-    ws.send('我不爱你')
+    console.log(`女朋友：${data}`)
+    if (/重要/.test(data)) {
+      ws.send('你重要')
+      console.log('我：你重要')
+    } else if (/掉到河里/.test(data)) {
+      ws.send('先救你')
+      console.log('我：先救你')
+    } else {
+      ws.send('多喝热水')
+      console.log('我：多喝热水')
+    }
   });
 })
+
+app.listen(3001)
