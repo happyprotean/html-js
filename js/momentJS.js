d194473795df8c1068276d729a03995a339c3c47
“fanhuanxi” <fanhuanxi@meituan.com>
2022-09-02 14:26:10 +0800

    test moment

--- js/momentJS.js
+++ js/momentJS.js
@@ -1,6 +1,5 @@
 import moment from 'moment'
 import dayjs from 'dayjs'
 
-let a = moment('2013-02')
-console.log(a.format('yyyy年M月'))
-console.log(moment('').format('yyyy年M月'))
+const now = dayjs().format('YYYYMMDDHHmmss')
+console.log('now', now)
